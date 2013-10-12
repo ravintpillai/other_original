1c49223d3f7e499da571cc5331cdda294295af53
Ravin Thambapillai <Ravin.Thambapillai@gmail.com>
2013-10-12 16:47:28 +0100

    titles refactored

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) {"Ravin Thambapillai"}
+
 	describe "Home page" do
 
 		it "should have the content 'Ravin Thambapillai'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     	it "should have the title 'Home'" do
     		visit '/static_pages/home'
-    		expect(page).to have_title("Ravin Thambapillai | Home")
+    		expect(page).to have_title("#{base_title} | Home")
     	end
   	end
 
@@ -24,7 +26,7 @@ describe "Static pages" do
 
   		it "should have the title  'Help'" do
   			visit "/static_pages/help"
-  			expect(page).to have_title("Ravin Thambapillai | Help")
+  			expect(page).to have_title("#{base_title} | Help")
   		end
   	end
 
@@ -37,14 +39,14 @@ describe "Static pages" do
 
   		it "should have the title 'About'" do
   			visit "/static_pages/about"
-  			expect(page).to have_title('Ravin Thambapillai | About')
+  			expect(page).to have_title("#{base_title} | About")
   		end
   	end
 
     describe "Contact page" do
       it "should have the title 'Contact'" do
         visit '/static_pages/contact'
-        expect(page).to have_title("Ravin Thambapillai | Contact")
+        expect(page).to have_title("#{base_title} | Contact")
       end
 
       it "should have the content 'Contact'" do
