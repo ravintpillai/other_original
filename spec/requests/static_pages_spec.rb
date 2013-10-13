7210505d5b85607eb04900768647c50936d8fd2f
Ravin Thambapillai <Ravin.Thambapillai@gmail.com>
2013-10-13 23:20:48 +0100

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,55 +3,34 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) {"Ravin Thambapillai"}
+  subject {page}
 
 	describe "Home page" do
+    before {visit root_path}
 
-		it "should have the content 'Ravin Thambapillai'" do
-			visit '/static_pages/home'
-			expect(page).to have_content('Ravin Thambapillai')
-    	end
+		it {should have_content('Ravin Thambapillai')}
+    it {should have_title(full_title(''))}
+    it {should_not have_title("| Home")}
+  end
 
-    	it "should have the title 'Home'" do
-    		visit '/static_pages/home'
-    		expect(page).to have_title("#{base_title} | Home")
-    	end
-  	end
+  describe "Help page" do
+      before {visit help_path}
 
-  	describe "Help page" do
-
-  		it "should have the content 'Help'" do
-  			visit "/static_pages/help"
-  			expect(page).to have_content('Help')
-  		end
-
-  		it "should have the title  'Help'" do
-  			visit "/static_pages/help"
-  			expect(page).to have_title("#{base_title} | Help")
-  		end
+  		it {should have_content('Help')}
+  		it {should have_title('Help')}
   	end
 
   	describe "About page" do
+      before {visit about_path}
 
-  		it "should have the content 'About'" do
-  			visit "/static_pages/about"
-  			expect(page).to have_content('About')
-  		end
-
-  		it "should have the title 'About'" do
-  			visit "/static_pages/about"
-  			expect(page).to have_title("#{base_title} | About")
-  		end
+  		it {should have_content('About')}
+      it {should have_title('About')}
   	end
 
     describe "Contact page" do
-      it "should have the title 'Contact'" do
-        visit '/static_pages/contact'
-        expect(page).to have_title("#{base_title} | Contact")
-      end
+      before {visit contact_path}
 
-      it "should have the content 'Contact'" do
-        visit '/static_pages/contact'
-        expect(page).to have_content('Contact')
-      end
+      it {should have_title('Contact')}
+      it { should have_content('Contact')}
     end
 end
