01af2dca441ccc2af833787cc06f2dd6c5b8e0cf
Ravin Thambapillai <Ravin.Thambapillai@gmail.com>
2013-10-15 00:30:38 +0100

    finish the user model

--- db/migrate/20131014192631_create_users.rb
+++ db/migrate/20131014192631_create_users.rb
@@ -3,8 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-
       t.timestamps
     end
   end
+
+  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.create(name: "Someguy", email: "some@guy.com")}
+  before { @user = User.create(name: "Someguy", email: "some@guy.com",
+  password: "password", password_confirmation: "password")}
 
   subject {@user}
 
   it {should respond_to(:name)}
   it {should respond_to(:email)}
+  it {should respond_to(:password_digest)}
+  it {should respond_to(:password)}
+  it {should respond_to(:password_confirmation)}
+
   it {should be_valid }
   
   describe "when name is not present" do
@@ -52,4 +57,37 @@ describe User do
   		expect(user_with_same_email).not_to be_valid
   		end
 	end
+
+  describe "when password is blank" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                     password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password is not the same as password_confirmation" do
+    before {@user.password_confirmation = "mismatch"}
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
 end
