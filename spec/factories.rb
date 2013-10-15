a5291a792324175a7d6d63a340e6e61d461de789
Ravin Thambapillai <Ravin.Thambapillai@gmail.com>
2013-10-15 23:31:53 +0100

    Add SSL in production

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Ravin Thambapillai"
+    email    "ravin.thambapillai@gmail.com"
+    password "password"
+    password_confirmation "password"
+  end
+end
